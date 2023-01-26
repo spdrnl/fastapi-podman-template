c9071422bde74a600e011ea4c729955e0dc0d7ab
Sanne de Roever <sanne.de.roever@gmail.com>
2023-01-26 14:23:07 +0100

    Added Click library for command line support.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
-
-app = FastAPI()
+import click
 
 
-@app.get("/")
-def read_root():
-    """Serve a hello world at the root"""
-    return {"Hello": "World"}
+@click.command()
+@click.option("--count", default=1, help="number of greetings")
+@click.argument("name")
+def hello(count, name):
+    """A simple greeting function."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+
+if __name__ == "__main__":
+    hello()
